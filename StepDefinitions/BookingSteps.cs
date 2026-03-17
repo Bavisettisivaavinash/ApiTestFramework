22b60e4ca29ca866eda5352d9f7999547a0b27b2
Bavisetti Siva Avinash <48872557+Bavisettisivaavinash@users.noreply.github.com>
2026-03-17 12:35:14 +0530

    Create StepDefinitions/BookingSteps.cs

---
+++ StepDefinitions/BookingSteps.cs
@@ -0,0 +1,10 @@
+using TechTalk.SpecFlow;
+
+namespace ApiTestFramework.StepDefinitions
+{
+    [Binding]
+    public class BookingSteps
+    {
+        // Step definitions for Booking features will go here
+    }
+}
