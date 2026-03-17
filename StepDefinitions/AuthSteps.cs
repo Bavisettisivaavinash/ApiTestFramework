34452111a0339dcb0278b1632b82a857c7a5cff3
Bavisetti Siva Avinash <48872557+Bavisettisivaavinash@users.noreply.github.com>
2026-03-17 12:35:13 +0530

    Create StepDefinitions/AuthSteps.cs

---
+++ StepDefinitions/AuthSteps.cs
@@ -0,0 +1,10 @@
+using TechTalk.SpecFlow;
+
+namespace ApiTestFramework.StepDefinitions
+{
+    [Binding]
+    public class AuthSteps
+    {
+        // Step definitions for Auth feature will go here
+    }
+}
