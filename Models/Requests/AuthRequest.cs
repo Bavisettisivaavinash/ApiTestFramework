82b3aa81a4dc1ccdc6b0942328eabcdb3ccbc59c
Bavisetti Siva Avinash <48872557+Bavisettisivaavinash@users.noreply.github.com>
2026-03-17 12:35:36 +0530

    Create Models/Requests/AuthRequest.cs

---
+++ Models/Requests/AuthRequest.cs
@@ -0,0 +1,8 @@
+namespace ApiTestFramework.Models.Requests
+{
+    public class AuthRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
