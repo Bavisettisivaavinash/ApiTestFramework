b519edacd104feba33d58901e05ae470dbea9a86
Bavisetti Siva Avinash <48872557+Bavisettisivaavinash@users.noreply.github.com>
2026-03-17 12:35:55 +0530

    Create Models/Common/BookingDates.cs

---
+++ Models/Common/BookingDates.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ApiTestFramework.Models.Common
+{
+    public class BookingDates
+    {
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
